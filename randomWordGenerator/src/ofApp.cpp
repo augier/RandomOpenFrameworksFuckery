57e17a02a4ca842a8a4a8ac3788aad05fb58a189
Chris Augier <chris.augier@gmail.com>
2017-09-11 23:05:18 +0100

    Adding ability to delete word with right click

--- randomWordGenerator/src/ofApp.cpp
+++ randomWordGenerator/src/ofApp.cpp
@@ -40,13 +40,19 @@ void ofApp::mouseDragged(int x, int y, int button){
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
     switch (button){
-    case OF_MOUSE_BUTTON_LEFT:
+        case OF_MOUSE_BUTTON_LEFT: {
             Word word;
             word.setup();
             words.push_back(word);
             break;
-//    case OF_MOUSE_BUTTON_RIGHT:
-            // do nothing;
+        }
+        case OF_MOUSE_BUTTON_RIGHT: {
+            // delete a random word
+            int i = ofRandom(words.size() - 1);
+            if (i < 0) return;
+            words.erase(words.begin()+i);
+            break;
+        }
     }
 }
 
